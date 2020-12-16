3759fb40dd5fd546e8c16754d50a1082248f0658
辻気流 <kiryu.18011106@gmail.com>
2020-12-16 15:07:39 +0900

    SpeechOneText デフォルト出力状態変更

--- main.py
+++ main.py
@@ -394,5 +394,5 @@ if __name__ == "__main__":
     # data = ReadConversationalData("test_data")
     # SpeechStart()
     # SpeechAnswer(data)
-    SpeechOneText()
+    print(SpeechOneText())
     PrintTime()
